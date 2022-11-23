aba061517a4fa8f1131c9fda4f44e8e7dd85401b
Alysson Marley Dias <callistomain@gmail.com>
2022-11-23 07:36:23 -0300

    core: db.js

---
+++ src/database/db.js
@@ -0,0 +1,15 @@
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Client connection
+const client = new MongoClient(process.env.MONGO_URI);
+try {
+  await client.connect();
+  console.log('MongoDB connected!');
+} catch (err) {
+  console.log('err.message');
+}
+
+// MongoDB
+const db = client.db('gameStore-dev');
